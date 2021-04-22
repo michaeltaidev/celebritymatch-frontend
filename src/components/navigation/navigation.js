f4b03d8b48f05b1283927472c94516060f024b95
Michael Tai <michaeltaidev@gmail.com>
2021-04-22 09:27:33 -0700

    Updated the title logo to a new font.

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -4,14 +4,14 @@ const Navigation = ({ changeRoute, isLoggedIn, logout }) => {
     if (isLoggedIn) {
       return (
         <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-          <h1 style={{ marginRight: 'auto' }} onClick={() => changeRoute('home')} className='link pa3 pointer'>Celebrity Match</h1>
+          <h1 style={{ marginRight: 'auto' }} onClick={() => changeRoute('home')} className='f2 link pa3 pointer'>Celebrity Match</h1>
           <p onClick={() => logout()} className='f4 link dim pa3 pointer'>Logout</p>
         </nav>
       )
     } else {
       return (
         <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-          <h1 style={{ marginRight: 'auto' }} onClick={() => changeRoute('home')} className='link pa3 pointer'>Celebrity Match</h1>
+          <h1 style={{ marginRight: 'auto' }} onClick={() => changeRoute('home')} className='f2 link pa3 pointer'>Celebrity Match</h1>
           <p onClick={() => changeRoute('login')} className='f4 link dim pa3 pointer'>Login</p>
           <p onClick={() => changeRoute('register')} className='f4 link dim pa3 pointer'>Register</p>
         </nav>
